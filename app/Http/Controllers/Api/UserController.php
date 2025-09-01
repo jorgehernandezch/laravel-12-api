e08f6b61da3805f87060a3ef297407282bbd2d70
Jorge Edo. Hernández <jorge@galpha.co>
2025-09-01 09:34:48 -0300

    feat(api): User Controller, Requests e Routes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,52 +3,120 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\User\UpdateBasicInfoRequest;
+use App\Http\Requests\Api\User\UpdateMinistryProfileRequest;
+use App\Http\Requests\Api\User\UpdatePasswordRequest;
+use App\Http\Requests\Api\User\UpdatePersonalDataRequest;
+use App\Http\Requests\Api\User\UpdateSocialProfileRequest;
+use App\Http\Resources\UserResource;
 use App\Http\Responses\ApiResponse;
-use App\Models\User;
+use App\Http\Traits\UserTrait;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Http\FormRequest;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    use UserTrait;
+
+    public function fetchAuthUser(Request $request)
     {
-        $users = User::all();
-        return ApiResponse::success('Users retrieved successfully', 200, [
-            'users' => $users
-        ]);
+        try {
+            $user = $request->user();
+
+            return ApiResponse::success('User retrieved successfully', 200, [
+                'user' => new UserResource($user)
+            ]);
+        } catch (\Exception $e) {
+            return ApiResponse::error('Failed to fetch user.', 500, [
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function updateBasicInfo(UpdateBasicInfoRequest $request)
     {
-        //
+        try {
+            $user = $request->user();
+            $user->update($request->validated());
+
+            return ApiResponse::success('User updated successfully', 200, [
+                'user' => new UserResource($user)
+            ]);
+        } catch (\Exception $e) {
+            return ApiResponse::error('Failed to update user.', 500, [
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function updatePassword(UpdatePasswordRequest $request)
     {
-        //
+        try {
+            $user = $request->user();
+            $user->update([
+                'password' => bcrypt($request->input('password'))
+            ]);
+
+            return ApiResponse::success('Password updated successfully', 200);
+        } catch (\Exception $e) {
+            return ApiResponse::error('Failed to update password.', 500, [
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function updatePersonalData(UpdatePersonalDataRequest $request)
     {
-        //
+        return $this->handleProfileUpdate($request);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function updateSocialProfile(UpdateSocialProfileRequest $request)
     {
-        //
+        return $this->handleProfileUpdate($request);
+    }
+
+    public function deactivateAccount(Request $request)
+    {
+        try {
+            $user = $request->user();
+            $user->update(['is_active' => false]);
+
+            return ApiResponse::success('Account deactivated successfully', 200);
+        } catch (\Exception $e) {
+            return ApiResponse::error('Failed to deactivate account.', 500, [
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function deleteAccount(Request $request)
+    {
+        try {
+            $user = $request->user();
+            $user->tokens()->delete();
+            $user->delete();
+
+            return ApiResponse::success('Account deleted successfully', 200);
+        } catch (\Exception $e) {
+            return ApiResponse::error('Failed to delete account.', 500, [
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    private function handleProfileUpdate(FormRequest $request)
+    {
+        try {
+            $user = $request->user();
+            $user->profile->update($request->validated());
+
+            return ApiResponse::success('User updated successfully', 200, [
+                'user' => new UserResource($user)
+            ]);
+        } catch (\Exception $e) {
+            return ApiResponse::error('Failed to update user.', 500, [
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,16 @@ Route::prefix('v1')->group(function () {
     Route::post('forgot-password-token', [AuthController::class, 'generateResetToken']);
     Route::post('reset-password', [AuthController::class, 'resetPassword']);
     Route::middleware('auth:sanctum')->group(function () {
-        Route::apiResource('users', UserController::class);
+        //Auth User Routes
+        Route::prefix('user')->group(function () {
+            Route::get('/', [UserController::class, 'fetchAuthUser']);
+            Route::put('/basic-info', [UserController::class, 'updateBasicInfo']);
+            Route::put('/password', [UserController::class, 'updatePassword']);
+            Route::put('/personal-data', [UserController::class, 'updatePersonalData']);
+            Route::put('/social-profile', [UserController::class, 'updateSocialProfile']);
+            Route::post('/deactivate', [UserController::class, 'deactivateAccount']);
+            Route::delete('/delete', [UserController::class, 'deleteAccount']);
+        });
         //Logout route
         Route::post('logout', [AuthController::class, 'logout']);
     });
